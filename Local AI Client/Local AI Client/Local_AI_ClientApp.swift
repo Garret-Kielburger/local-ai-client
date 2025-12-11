400bc0d2c23b7d2669c42bb4ca65b48cafe8145a
Garret Kielburger <garret.kielburger@gmail.com>
2025-12-11 12:30:07 -0500

    Initial commit

---
+++ Local AI Client/Local AI Client/Local_AI_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_AI_ClientApp.swift
+//  Local AI Client
+//
+//  Created by Garret Kielburger on 2025-12-11.
+//
+
+import SwiftUI
+
+@main
+struct Local_AI_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Local AI Client/Local AI ClientTests/Local_AI_ClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  Local_AI_ClientTests.swift
+//  Local AI ClientTests
+//
+//  Created by Garret Kielburger on 2025-12-11.
+//
+
+import Testing
+@testable import Local_AI_Client
+
+struct Local_AI_ClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
